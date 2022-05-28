f66a278ff0e5a6431f8e6c49c147af1be57e52e2
Bellatrix <bellatrixdesign22@gmail.com>
2022-05-28 11:41:58 -0300

    ItemListContainer

--- marlo/src/App.js
+++ marlo/src/App.js
@@ -1,22 +1,23 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <ItemListContainer greeting="Hola Coders" />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Bienvenidos a Marló</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ver más
         </a>
       </header>
     </div>
